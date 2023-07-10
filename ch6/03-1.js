395c2af02fdfd75fbba7089dadac54e9028e0638
chiman <rlaclghks123@naver.com>
2023-07-10 13:47:27 +0900

    6.3 변수 추출하기 연습 완료

--- ch6/03-1.js
+++ ch6/03-1.js
@@ -1,19 +1,19 @@
-const price = order => {
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  )
-}
+const price = (order) => {
+  const 기본가격 = order.quantity * order.itemPrice;
+  const 수량확인 = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const 배송비 = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+
+  return 기본가격 - 수량확인 + 배송비;
+};
 
 const orderA = {
   itemPrice: 600,
   quantity: 3,
-}
+};
 const orderB = {
   itemPrice: 8000,
   quantity: 2,
-}
+};
 
-console.log(price(orderA))
-console.log(price(orderB))
+console.log(price(orderA));
+console.log(price(orderB));
--- ch6/03-2.js
+++ ch6/03-2.js
@@ -1,30 +1,39 @@
 class Order {
   constructor(aRecord) {
-    this._data = aRecord
+    this._data = aRecord;
   }
   get quantity() {
-    return this._data.quantity
+    return this._data.quantity;
   }
   get itemPrice() {
-    return this._data.itemPrice
+    return this._data.itemPrice;
   }
+
+  get 기본가격() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get 수량확인() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get 배송비() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
+  }
+
   get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    )
+    return this.기본가격 - this.수량확인 + this.배송비;
   }
 }
 
 const orderA = new Order({
   itemPrice: 600,
   quantity: 3,
-})
+});
 const orderB = new Order({
   itemPrice: 8000,
   quantity: 2,
-})
+});
 
-console.log(orderA.price)
-console.log(orderB.price)
+console.log(orderA.price);
+console.log(orderB.price);
