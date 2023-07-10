4b4adf5a004ff42979a2108d57c2518a5a0c9c2a
chiman <rlaclghks123@naver.com>
2023-07-10 13:46:15 +0900

    6.1 함수 추출하기 연습 완료

--- ch6/01.js
+++ ch6/01.js
@@ -1,21 +1,31 @@
-const printOwing = invoice => {
-  let outstanding = 0
+const printBanner = () => {
+  console.log('*******************');
+  console.log('***** 고객채무 *****');
+  console.log('*******************');
+};
 
-  console.log('*******************')
-  console.log('***** 고객채무 *****')
-  console.log('*******************')
+const printDetail = (invoice, outstanding) => {
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate?.toLocaleString()}`);
+};
 
+const calulateOutstanding = (invoice) => {
+  let result = 0;
   for (const o of invoice.orders) {
-    outstanding += o.amount
+    result += o.amount;
   }
+  return result;
+};
 
-  const today = new Date()
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+const printOwing = (invoice) => {
+  printBanner();
 
-  console.log(`고객명: ${invoice.customer}`)
-  console.log(`채무액: ${outstanding}`)
-  console.log(`마감일: ${invoice.dueDate?.toLocaleString()}`)
-}
+  const today = new Date();
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+  printDetail(invoice, calulateOutstanding(invoice));
+};
 
 printOwing({
   customer: '재남',
@@ -23,4 +33,4 @@ printOwing({
     { name: '사채', amount: 100 },
     { name: '대출', amount: 1000 },
   ],
-})
+});
