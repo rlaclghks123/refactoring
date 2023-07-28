8fe2c7949011dc30e41ddae727ed3887aa2ed5f3
chiman <rlaclghks123@naver.com>
2023-07-28 11:29:46 +0900

    7.6 클래스 인라인하기 완료

---
+++ ch7/7.6클래스인라인하기/리팩토링후.js
@@ -0,0 +1,32 @@
+// 불필요하다고 판단한 TrackingInformation 클래스를 Shipment 클래스로 인라인 하기
+class Shipment {
+  _shippingCompany;
+  _trackingNumber;
+
+  get trackingInfo() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
+  }
+
+  get shippingCompany() {
+    return this._shippingCompany;
+  }
+  set shippingCompany(arg) {
+    this._shippingCompany = arg;
+  }
+  get trackingNumber() {
+    return this._trackingNumber;
+  }
+  set trackingNumber(arg) {
+    this._trackingNumber = arg;
+  }
+}
+
+const client1 = () => {
+  const aShipment = new Shipment();
+  const vendor = { name: 'A-SHIP', number: '010-1234-5678' };
+  aShipment.shippingCompany = vendor.name;
+  aShipment.trackingNumber = vendor.number;
+  return aShipment.trackingInfo;
+};
+
+console.log(client1());
